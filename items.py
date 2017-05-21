0b515dc45d7974e7fc7d0012fcf88e58887d32b3
C Becker <caroline.becker@wikimedia.fr>
2017-05-21 14:05:35 +0200

    Cached

--- items.py
+++ items.py
@@ -2,13 +2,17 @@ import json
 import requests
 import re
 import datetime
+from datetime import datetime
+from datetime import timedelta
 import pywikibot
 from pywikibot import page
 
 commons = pywikibot.Site('commons', 'commons')
 commonsedge = "https://tools.wmflabs.org/commonsedge/api.php?file="
 itemExpression = re.compile("Q\d+")
-cache = open("cachedItems.json").read()
+cache = json.loads(open("dump.json").read())
+delta = timedelta(days=150)
+dateFormat = "%Y-%m-%d %H:%M:%S.%f"
 
 def loads_items(category, depth=2):
     items = []
@@ -20,10 +24,10 @@ def loads_items(category, depth=2):
     return items
 
 def now():
-    return {"Timestamp":datetime.datetime.now()}
+    return {"Timestamp":str(datetime.now())}
 
 def sub(category, depth=1):
-    files = page0.Category(commons, category).articlesList()
+    files = page.Category(commons, category).articlesList()
     if depth <= 0:
         return files
     else:
@@ -34,44 +38,67 @@ def sub(category, depth=1):
             result = result+sub(cat.title(), depth-1)
         return result
 
-def institution(categoryName, height=4):
-    category = page.Category(commons, categoryName)
-    if height <= 0:
-        inst = [i for i in category.articles(namespaces=106)]
-        if len(inst) == 0:
-            return None
-        else:
-            items = itemExpression.findall(inst[0].get())
-            if len(items) == 0:
-                return None
-            else:
-                return items[0]
+def isExpired(property):
+    return "Timestamp" not in property.keys()
+    or datetime.now() -datetime.strptime(property["Timestamp"], "%Y-%m-%d %H:%M:%s.%Z") > delta
+
+def oldInstitution(categoryName):
+    if (categoryName in cache.keys()
+        and "Properties" in cache[categoryName]
+        and "P195" in cache[categoryName]["Properties"]
+        and (not isExpired(cache[categoryName]["Properties"]["P195"]))):
+        return cache[categoryName]["Properties"]["P195"]["Value"]
     else:
-        result = institution(categoryName, 0)
-        if result is None:
+        return "not found"
+
+def institution(categoryName, height=4, stores=True):
+    print categoryName
+    print height
+    result = oldInstitution(categoryName)
+    if result == "not found":
+        category = page.Category(commons, categoryName)
+        if height <= 0:
+            inst = [i for i in category.articles(namespaces=106)]
+            if len(inst) == 0:
+                result = None
+            else:
+                items = itemExpression.findall(inst[0].get())
+                if len(items) is not 0:
+                    result = items[0]
+        else:
+            result = institution(categoryName, 0, True)
             for parent in category.categories():
-                result = institution(parent.title(), height-1)
                 if result is not None:
                     break
-        return result
+                result = institution(parent.title(), height-1)
+        print "Storing"
+        fill(categoryName, result, cache)
+    else:
+        print "Already found"
+        result = None
+    print "result"
+    print result
+    return result
 
 def value(item):
-    return {"Value":item}
+    return {"Value":item,"Timestamp":datetime.now()}
 
 def fill(category, item, result):
-    print category
-    print item
-    dict={}
-    dict["P195"] = value(item)
-    dict["P276"] = value(item)
-    result[category] = dict
+    if category not in result.keys() or "P195" not in result[category].keys() or result[category]["P195"]["Value"] is None:
+        dict={}
+        dict["P195"] = value(item)
+        dict["P276"] = value(item)
+        result[category] = {"Properties":dict}
 
 def institutions(categoryName):
-    result = {}
     category = page.Category(commons, categoryName)
     for subPage in sub(categoryName):
         if subPage.isCategory():
-            fill(subPage.title(),institution(subPage.title()), result)
+            institution(subPage.title(0), stores=True)
+            print "Institution found"
+    print "Institutions"
+    with open("dumpR.json", "w") as data:
+        data = json.dumps(cache, indent=2)
     return result
 
 def item(page):
@@ -96,3 +123,5 @@ def item(page):
     except pywikibot.data.api.APIError:
         return [False, "API Error"]
 
+#institutions("Paintings by Carlo Crivelli by museum")
+institutions("Paintings by Aelbert Cuyp by museum")
