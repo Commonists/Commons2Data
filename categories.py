f6048d3093772e93f670d0856c83738c698eb9be
Caroline Lena <caroline.becker@wikimedia.fr>
2018-01-18 03:02:39 +0100

    Works now with existing categories

--- categories.py
+++ categories.py
@@ -26,12 +26,13 @@ def flush():
     with open ("tree.json", "w") as data:
         json.dump(tree, data)
 
-def build_tree(category, with_children=True, with_parents=False):
+def build_tree(category, children_depth=0, parents_depth=0, with_files=False):
     global nbCalls
     nbCalls = nbCalls + 1
     if (nbCalls % frequency) == 0:
         flush()
     if category not in tree:
+        LOG.info(category)
         node = {}
         parents = [p.title() for p in
         page.Category(commons, category).categories()]
@@ -40,21 +41,51 @@ def build_tree(category, with_children=True, with_parents=False):
         page.Category(commons, category).subcategories()]
         node["Children"] = children
         tree[category] = node
-        if with_children:
+        if children_depth > 0:
             for child in children:
-                build_tree(child, with_children, with_parents)
-        if with_parents:
+                build_tree(child, children_depth-1, parents_depth)
+        if parents_depth > 0:
             for parent in parents:
-                build_tree(parent, with_children, with_parents)
+                build_tree(parent, with_children, parents_depth-1)
+        if with_files:
+            for file in page.Category(commons, category).fil
+
+def paintings_by_year():
+    taboo=[u"Paintings by artist by year‎",
+"Paintings by country by year‎",
+"Paintings by genre by year‎",
+"Paintings by style by year‎",
+"Paintings by technique by year‎",
+"Paintings by year by artist‎",
+"Paintings by year by country‎",
+"Paintings not categorised by year‎",
+"Paintings with years of production (artist)‎",
+"Paintings of people by year‎",
+"Paintings from the United States by year‎"]
+'''    "Properties": {
+      "P31": {
+        "Value": "Q3305213"
+      },
+      "P571": {
+        "Value": {
+          "Year":1912
+          }
+        }'''
+    motherCat = page.Category(commons, "Paintings by year")
+    for cat in motherCat.categories():
+        if cat.title() not in taboo:
+            tree[cat.title()]
 
 def main():
-    categories =[u"Paintings by Vincent van Gogh in the Art Institute of Chicago"]
+    categories =[u"Lena temp2"]
     if len(tree) > 0:
         LOG.info("Already %d elements",len(tree))
-        LOG.info("Building tree")
-        for category in categories:
-            build_tree(category, with_children=True, with_parents=False)
-        flush()
+    LOG.info("Building tree")
+    for category in categories:
+        build_tree(category, children_depth=1, parents_depth=1, with_files=True)
+    flush()
 
 if __name__ == "__main__":
     main()
+
+
--- categorisation.py
+++ categorisation.py
@@ -16,6 +16,10 @@ repo = wikidata.data_repository()
 
 itemExpression = re.compile("Q\d+")
 
+dict_creator = {}
+
+missing = []
+
 cache = json.loads(open("dump.json").read())
 
 #Properties
@@ -24,20 +28,19 @@ inventory = "P217"
 commonsCat = "P373"
 imageProperty = "P18"
 depict = "P180"
+creator = "P170"
 
-duplicates=[depict]
+duplicates=[depict, creator]
 
 FILE_NAMESPACE = 6
+CREATOR_NAMESPACE = 100
 
-label_dict={"en":"The Little Blue Horses", "de":"Die Kleinen Blauen Pferde", "sv":"Blå hästar"}
-descr_dict={"en":"Franz Marc painting", "fr":"Peinture de Franz Marc"}
-
-blackList=[""]
+blackList=["Category:Rituels grecs – Une expérience sensible","Category:Details of paintings by Georges de La Tour"]
 
 def hidden(category):
     return "Category:Hidden categories" in [c.title() for c in category.categories()]
 
-def fusion_cat(tempCat,qitem=""):
+def fusion_cat(tempCat,qitem="", cat_name="", label_dict={}, descr_dict={}, objectCat=True, createCat=True):
     categories=[]
     img = None
     item = None
@@ -45,33 +48,47 @@ def fusion_cat(tempCat,qitem=""):
     for image in page.members(namespaces=FILE_NAMESPACE):
         img = image.title()[5:]
         for cat in image.categories():
-            if cat.title() not in blackList and not hidden(cat):
-                categories.append(cat.title())
-                blackList.append(cat.title())
+            if createCat:
+                if cat.title() not in blackList and not hidden(cat):
+                    categories.append(cat.title())
+                    blackList.append(cat.title())
+            elif not hidden(cat):
+                for parent in cat.categories():
+                    if parent not in blackList:
+                        categories.append(parent.title())
+                        blackList.append(parent.title())
     if qitem is not "":
         item = pywikibot.ItemPage(repo,qitem)
+        item.get()
     else:
         item = pywikibot.ItemPage(wikidata)
         item.editLabels(label_dict, summary="#Commons2Data label")
         item.editDescriptions(descr_dict, summary="#Commons2Data description")
         item.get()
-        for cat in categories:
-            if cat in cache:
-                for p in cache[cat]["Properties"]:
-                    if p not in item.claims or p in duplicates:
-                        claim = pywikibot.Claim(repo, p)
+    for cat in categories:
+        if cat in cache:
+            for p in cache[cat]["Properties"]:
+                if p not in item.claims or p in duplicates:
+                    claim = pywikibot.Claim(repo, p)
+                    if "Value" in cache[cat]["Properties"][p]:
                         if "Q" in cache[cat]["Properties"][p]["Value"]:
                             claim.setTarget(pywikibot.ItemPage(repo,cache[cat]["Properties"][p]["Value"]))
                         else:
                             claim.setTarget(pywikibot.WbTime(year=cache[cat]["Properties"][p]["Value"]["Year"]))
                         item.addClaim(claim, summary=u'#Commons2Data adding claim')
-            else:
-                print (cat)
-    title = label(item)
-    print_category(item.title(), title, categories)
-    categories.append("Category:"+tempCat)
-    for image in page.members(namespaces=FILE_NAMESPACE):
-        clean_image(image, title, categories)
+                    else:
+                        print (cat)
+                        print (p)
+        else:
+            print (cat)
+    title = cat_name
+    if title is "":
+        title = label(item)
+    if createCat:
+        print_category(item.title(), title, categories,objectCat)
+        categories.append("Category:"+tempCat)
+        for image in page.members(namespaces=FILE_NAMESPACE):
+            clean_image(image, title, categories)
     # Wikidata
     if imageProperty not in item.claims:
         claim = pywikibot.Claim(repo, imageProperty)
@@ -85,23 +102,27 @@ def fusion_cat(tempCat,qitem=""):
 
 def label(item):
     title=""
-    item.get()
     if u"en" in item.labels:
         title = item.labels["en"]
-        if catalog in item.claims:
-            if item.claims[catalog][0].target is not None:
-                title = title+" ("+item.claims[catalog][0].target+")"
-            elif item.claims[catalog][1].target is not None:
-                title = title+" ("+item.claims[catalog][1].target+")"
-        elif inventory in item.claims:
-            title = title+" ("+item.claims[inventory][0].target+")"
+    elif u"fr" in item.labels:
+        title = item.labels["fr"]
+    if catalog in item.claims:
+        if item.claims[catalog][0].target is not None:
+            title = title+" ("+item.claims[catalog][0].target+")"
+        elif item.claims[catalog][1].target is not None:
+            title = title+" ("+item.claims[catalog][1].target+")"
+    elif inventory in item.claims:
+        title = title+" ("+item.claims[inventory][0].target+")"
+    elif creator in item.claims:
+        itemAuthor = item.claims[creator][0].target
+        itemAuthor.get()
+        title = title+" ("+itemAuthor.labels["en"]+")"
     return title
 
-
-def print_category(item, title, addList):
+def print_category(item, title, addList, objectCat=True):
     if title is not "":
         result = ""
-        if item is not "":
+        if item is not "" and objectCat:
             result = "<onlyinclude>\n{{User:Rama/Catdef|"+item+"}}\n</onlyinclude>"
         category = pywikibot.Category(commons, title)
         for add in addList:
@@ -112,9 +133,49 @@ def print_category(item, title, addList):
 def clean_image(image, title, removeList):
     t = image.text
     for r in removeList:
-        t = t.replace("[["+r+"]]","")
+        pattern = re.compile("\[\["+r+"(\|\w+)?\]\]")
+        s = re.search(pattern, t)
+        if s is not None:
+            t = t.replace(s.group(0),"")
     t = t+"\n[[Category:"+title+"]]"
     image.text = t
     image.save("#FileToCat Image in its own category")
 
-fusion_cat("Lena temp1", "Q30079741")
+def creator_of(category):
+    try:
+        creator = category.members(namespaces=CREATOR_NAMESPACE).next()
+        if re.search(itemExpression, creator.text) is not None:
+            return re.search(itemExpression, creator.text).group(0)
+        else:
+            return None
+    except StopIteration:
+        return None
+
+def creators_of(category_name):
+    category = pywikibot.Category(commons, category_name)
+    for subcat in category.subcategories():
+        item = creator_of(subcat)
+        if item is not None:
+            dict_creator[subcat.title()]={
+            "Properties":{"P170":{"Value":item.title()}},
+            "Parents":[category_name]}
+        else:
+            missing.append(subcat.title())
+    with open ("creators.json", "w") as data:
+        json.dump(dict_creator, data, indent=2, ensure_ascii=False)
+    with open ("missing.json", "w") as data:
+        json.dump({"Missing":missing},data, indent=2, ensure_ascii=False)
+
+def item_of(category_name):
+    category = pywikibot.Category(commons, category_name)
+
+def item_of(file):
+    result = []
+
+
+#creators_of("Attic black-figure vase-painters")
+
+fusion_cat("Lena temp4", "", "The Penitent Magdalen",
+    {"en":"The Penitent Magdalen,"},
+    {"en":"Georges de la Tour painting"},
+    True, True)
