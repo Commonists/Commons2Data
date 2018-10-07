689f31a15a7bef83cc8b607ebd709ec4019363f5
Caroline Becker <caroline.becker@wikimedia.fr>
2018-10-07 15:17:58 +0200

    Now able to translate information from Artwork templates into Wikidata

--- categorisation.py
+++ categorisation.py
@@ -45,6 +45,7 @@ FILE_NAMESPACE = 6
 CREATOR_NAMESPACE = 100
 
 blackList=["Category:Rituels grecs – Une expérience sensible","Category:Details of paintings by Georges de La Tour"]
+blackListLang=[u'lang', u'1', u'title', u'Title']
 
 LOG =  logging.getLogger(name=__name__)
 HANDLER = logging.StreamHandler(stream=sys.stdout)
@@ -54,54 +55,93 @@ LOG.addHandler(HANDLER)
 LOG.setLevel(logging.DEBUG)
 
 def sanitize(word):
-    return word.replace("'","")
+    if type(word) is unicode:
+        return word.replace("'","")
+    else:
+        return word['name'].replace("'","")
 
-def institution(d):
-    inst = None
-    if 'institution' in d:
-        inst = d['institution'][0][0]['name']
-    elif 'Institution' in d:
-        inst = d['Institution'][0][0]['name']
-    if inst is not None:
-        page = pywikibot.Page(commons, inst)
+def templateToQitem(d, keys):
+    pageName = None
+    for key in keys:
+        if key in d:
+            pageName = d[key][0][0]['name']
+            break
+    if pageName is not None:
+        page = pywikibot.Page(commons, pageName)
         if page.isRedirectPage():
             page = page.getRedirectTarget()
-        return re.search(itemExpression, page.text).group(0)
+        return [re.search(itemExpression, page.text).group(0), re.split(":", pageName)[1]]
     else:
         return None
 
+def createur(d):
+    return templateToQitem(d, ["artist", "Artist"])
+
+def institution(d):
+    return templateToQitem(d, ["institution", "Institution"])
+
+def harvestMultilang(d):
+    result = {}
+    for element in d:
+        if 'params' in element[0]:
+            for key in element[0]['params']:
+                if key not in blackListLang:
+                    result[key] = sanitize(element[0]['params']['en'][0][0])
+            if 'lang' in element[0]['params']:
+                result[element[0]['params']['lang'][0][0].lower()]=element[0]['params']['1'][0][0]
+            if 'name' in element[0]:
+                key = element[0]['name'].lower()
+                if key not in blackListLang:
+                    result[element[0]['name'].lower()]=element[0]['params']['1'][0][0]
+    return result
+
 def harvestPage(filename):
-    json=requests.get(commonsedge+filename).json()
+    content=requests.get(commonsedge+filename).json()
     result={}
-    if "error" in json and "Artwork" in json["error"]:
-        d = json["error_data"][0]["params"]
+    if "error" in content and "Artwork" in content["error"]:
+        d = content["error_data"][0]["params"]
         if "description" in d:
             if "name" in d["description"][0][0]:
                 lang = d["description"][0][0]["name"].lower()
-                title = sanitize(d["description"][0][0]["params"]["1"][0][0])
-                result["label"]={lang:title}
+                if lang not in blackListLang:
+                    title = sanitize(d["description"][0][0]["params"]["1"][0][0])
+                    result["label"]={lang:title}
             else:
                 result["label"]={"en":sanitize(d["description"][0][0])}
         if "Title" in d:
-            t = d["Title"][0][0]["params"]
-            result["label"]={}
-            for key in t:
-                result["label"][key]=sanitize(t[key][0][0])
+            result["label"]=harvestMultilang(d["Title"])
         if "title" in d:
-            if 'params' in d["title"][0][0]:
-                t = d["title"][0][0]["params"]
-                result["label"]={}
-                for key in t:
-                    if key not in [u'lang', u'1']:
-                        result["label"][key]=sanitize(t[key][0][0])
-            else:
-                result["label"]={"en":sanitize(d["title"][0][0])}
+            result["label"]=harvestMultilang(d["title"])
         inst = institution(d)
         if inst:
-            result[collection]=inst
-            result[localization]=inst
+            result[collection]=inst[0]
+            result[localization]=inst[0]
+        artist = createur(d)
+        if artist:
+            result[creator] = artist[0]
+            result['artist'] = '('+artist[1]+')'
     return result
 
+def createItem(info, filename="", qitem=""):
+    if qitem is not "":
+        item = pywikibot.ItemPage(repo,qitem)
+        item.get()
+    else:
+        item = pywikibot.ItemPage(wikidata)
+        LOG.info("Languages of labels")
+        for lang in info["label"]:
+            LOG.info(lang)
+        item.editLabels(info["label"], summary="#Commons2Data label")
+        item.get()
+    for p in info:
+        if p not in ["label", "artist"] and p not in item.claims:
+            claim = pywikibot.Claim(repo, p)
+            claim.setTarget(pywikibot.ItemPage(repo,info[p]))
+            item.addClaim(claim, summary=p)
+    if imageProperty not in item.claims:
+        claim = pywikibot.Claim(repo, imageProperty)
+        claim.setTarget(pywikibot.FilePage(commons,filename))
+        item.addClaim(claim, summary=u'Image')
 
 def hidden(category):
     return "Category:Hidden categories" in [c.title() for c in category.categories()]
@@ -130,9 +170,11 @@ def fusion_cat(images,qitem="", cat_name="", label_dict={}, descr_dict={}, objec
         item.get()
     else:
         item = pywikibot.ItemPage(wikidata)
-        item.editLabels(info["label"], summary="#Commons2Data label")
-        item.editDescriptions(descr_dict, summary="#Commons2Data description")
-        item.get()
+        LOG.info("Languages of labels")
+        for lang in info["label"]:
+            item.editLabels(info["label"], summary="#Commons2Data label")
+            item.editDescriptions(descr_dict, summary="#Commons2Data description")
+            item.get()
     for cat in categories:
         if cat in cache:
             for p in cache[cat]["Properties"]:
@@ -144,8 +186,12 @@ def fusion_cat(images,qitem="", cat_name="", label_dict={}, descr_dict={}, objec
                         else:
                             claim.setTarget(pywikibot.WbTime(year=cache[cat]["Properties"][p]["Value"]["Year"]))
                         item.addClaim(claim, summary=u'#Commons2Data adding claim')
-    title = info["label"]["en"]
-    LOG.info(title)
+    if "label" in info and "en" in info["label"]:
+        title = info["label"]["en"]
+    else:
+        title = info["label"]["fr"]
+    if creator in info:
+        title = title + info['artist']
     if createCat:
         print_category(item.title(), title, categories,objectCat)
         categories.append(blackList[0])
@@ -189,16 +235,25 @@ def clean_image(image, title, removeList):
 
 def main():
     file_name = "Category:Lena temp1"
+    temp = False
     if len(sys.argv) > 1:
         file_name = sys.argv[1]
     p = pywikibot.Page(commons, file_name)
     if p.isCategory():
-        LOG.info("Examining files on temp category %s", file_name)
-        blackList.insert(0, file_name)
         cat = pywikibot.Category(p)
-        fusion_cat([m for m in cat.members(namespaces=FILE_NAMESPACE)],
-            cat_name="",
-            qitem="Q56880840")
+        if temp:
+            LOG.info("Examining files on temp category %s", file_name)
+            blackList.insert(0, file_name)
+            fusion_cat([m for m in cat.members(namespaces=FILE_NAMESPACE)],
+                cat_name="",
+                qitem="")
+        else:
+            LOG.info("Importing every image of category %s", file_name)
+            for image in cat.members(namespaces=FILE_NAMESPACE):
+                img = image.title()[5:]
+                LOG.info("Creating item for %s", img)
+                info = harvestPage(img)
+                createItem(info, img)
     else:
         LOG.info("Examining galleries on page %s", file_name)
         soup = BeautifulSoup(p.text, 'html.parser')
